87d41ff2c0b2a38d594ded53cc9aae040bf08982
andreicioaca <cioaca.andrei26@gmail.com>
2023-02-02 22:19:47 +0200

    exercise nr.2

---
+++ Methods/exercise-02/exercise.py
@@ -0,0 +1,18 @@
+num1 = 1.3
+num2 = 2.3
+num3 = 1j 
+num4 = 1.4 
+num5 = 1.5
+
+num1 = float(num1)
+num2 = int(num2)
+num3 = complex(num3)
+num4 = round(num4)
+num5 = round(num5)
+
+print(num1, num2, num3, num4, num5), 
+print (type(num1)), 
+print (type(num2)),
+print (type(num3)),
+print (type(num4)),
+print (type(num5));
