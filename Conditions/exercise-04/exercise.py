d7ed2c7a692282d28a8abb7963cbc6ad42c76018
andreicioaca <cioaca.andrei26@gmail.com>
2023-02-03 04:02:30 +0200

    exercise nr.4

--- Conditions/exercise-04/exercise.py
+++ Conditions/exercise-04/exercise.py
@@ -4,5 +4,11 @@ number1 = random.randint(-100,100)
 number2 = random.randint(-100,100)
 
 if abs(number1) > abs(number2):
-    __
+    print(f"{number1}\'s absolute value is greater than {number2}\'s absolute value")
+elif abs(number1) < abs(number2):
+    print(f"{number2}\'s absolute value is greater than {number1}\'s absolute value")    
+else:
+    print(f"{number1}\'s absolute value is equals to {number2}\'s absolute value")
+
+    
     
